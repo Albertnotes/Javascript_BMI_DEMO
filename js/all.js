ef700086cbe3065c306d9f703d7ea391905515a4
Albert <sab4856@gmail.com>
2020-02-14 20:40:23 +0800

    v2 修改版

--- js/all.js
+++ js/all.js
@@ -1,113 +1,93 @@
 // DOM
-var heightId = document.querySelector('#heightId');
-var weightId = document.querySelector('#weightId');
-var countId = document.querySelector('#countId');
-var list = document.querySelector('#list');
-var del = document.querySelector('#del');
-var data = JSON.parse(localStorage.getItem('listData')) || [];
-
-
-// event init
-listHtml(data);
-
-// event
-countId.addEventListener('click', eventDate, false);
-del.addEventListener('click', delallData, false);
-
-function eventDate(e) {
-    if (e.target.id === 'resultValue') {
-        e.preventDefault();
-        var w = parseInt(weightId.value);
-        var h = parseInt(heightId.value);
-        console.log(w)
-        if (isNaN(w) || isNaN(h)) {
-            alert('請輸入數值哦 ( ˘•ω•˘ ).oOஇ')
-            return
-        };
-        bmiCount(w, h);
-        bmiLevel(bmi);
-        upDateArray(w, h, bmi, level);
-        resultHtml();
-        listHtml(data);
-    }
-    else if (e.target.id === 'resetValue') {
-        resetHtml();
-        // localStorage.setItem('listData', JSON.stringify(data));
-        // listHtml(data);
-    }
-}
-
-function delallData(e) {
-    e.preventDefault();
-    data = [];
-    localStorage.setItem('listData', JSON.stringify(data));
-    del.classList.add('h-d-none');
-    listHtml(data);
-}
+const countId = document.querySelector('#countId');
+const list = document.querySelector('#list');
+const del = document.querySelector('#del');
+let data = JSON.parse(localStorage.getItem('listData')) || [];
 
 // Model
+let bmiLevelData = {};
+let bmiLevelClass = {
+    'thin': {
+        class: "alert--1",
+        message: "過輕"
+    },
+    'normal': {
+        class: "alert--2",
+        message: "理想"
+    },
+    'heavy': {
+        class: "alert--3",
+        message: "過重"
+    },
+    'mildFat': {
+        class: "alert--4",
+        message: "輕度肥胖"
+    },
+    'moderateFat': {
+        class: "alert--4",
+        message: "中度肥胖"
+    },
+    'severeFat': {
+        class: "alert--5",
+        message: "重度肥胖"
+    },
+};
+
 function bmiCount(weight, height) {
     height = height / 100;
-    bmi = weight / (height * height);
-    bmi = bmi.toFixed(2);
-    // console.log(BMI);
-    return bmi;
+    let bmi = (weight / (height * height)).toFixed(2);
+    switch (true) {
+        case bmi < 18.5:
+            bmiLevelData.level = 'thin';
+            break
+        case bmi >= 18.5 && bmi < 24:
+            bmiLevelData.level = 'normal';
+            break
+        case bmi >= 24 && bmi < 27:
+            bmiLevelData.level = 'heavy';
+            break
+        case bmi >= 27 && bmi < 30:
+            bmiLevelData.level = 'mildFat';
+            break
+        case bmi >= 30 && bmi < 35:
+            bmiLevelData.level = 'moderateFat';
+            break
+        case bmi >= 35:
+            bmiLevelData.level = 'severeFat';
+            break
+    }
+    return bmi
 }
 
-function upDateArray(weight, height, bmi) {
-    var dataObject = {
+function upDateArray(weight, height, bmi, time) {
+    let dataObject = {
         weight: weight,
         height: height,
         bmi: bmi,
-        level: level,
+        level: bmiLevelData.level,
+        time: time,
     }
     data.push(dataObject);
     localStorage.setItem('listData', JSON.stringify(data));
 }
 
-function bmiLevel(value) {
-    if (value >= 18.5 && value < 24) {
-        level = '理想';
-        countId.classList.add("alert--2");
-    }
-    else if (value >= 24 && value < 27) {
-        level = '過重'
-        countId.classList.add("alert--3");
-    }
-    else if (value >= 27 && value < 30) {
-        level = '輕度肥胖'
-        countId.classList.add("alert--4");
-    }
-    else if (value >= 30 && value < 35) {
-        level = '中度肥胖'
-        countId.classList.add("alert--4");
-    }
-    else if (value >= 35) {
-        level = '重度肥胖'
-        countId.classList.add("alert--5");
-    }
-    else {
-        level = '過輕'
-        countId.classList.add("alert--1");
-    }
-    return level;
-}
-
 //view
-function resultHtml() {
-    var result = `
+function resultHtml(bmi) {
+    let str = bmiLevelData.level;
+    let result = `
     <div class="circle">
         <a href="#" class="resetButton" id="resetValue"></a>
             <div class="valuebox">
-                <p class="circleValue">`+ bmi + `</p>
+                <p class="circleValue">${bmi}</p>
                 <p class="circleValue">BMI</p>
             </div>
     </div>
-    <p class="resultText">`+ level + `</p>`;
+    <p class="resultText">${bmiLevelClass[str].message}</p>`;
+    countId.classList.add(bmiLevelClass[str].class);
     countId.innerHTML = result;
 }
 function resetHtml() {
-    var reset = `
+    let reset = `
         <a href="#" class="circle h2" id="resultValue">看結果</a>
     `;
     countId.innerHTML = reset;
@@ -117,90 +97,69 @@ function resetHtml() {
 
 // 執行帶入 data 判斷是否有陣列資料
 function listHtml(listData) {
-    var dataLength = listData.length;
-    var listHtml = '';
-    var today = new Date();
-    for (var i = 0; i < dataLength; i++) {
-        if (data[i].level === '理想') {
-            listHtml += `
-            <div class="recordBar recordBar--alert-2 h-bg-white">
-                <span class="recordBar__item h3">`+ data[i].level + `</span>
-                <span class="recordBar__item h3 bmi">`+ data[i].bmi + `</span>
-                <span class="recordBar__item h3 weight">`+ data[i].weight + `</span>
-                <span class="recordBar__item h3 height">`+ data[i].height + `</span>
-                <span class="recordBar__item h6">`
-                +(today.getMonth() + 1) + '-' + today.getDate() + '-' + today.getFullYear()+
-                `</span>
+    let dataLength = listData.length;
+    let listHtml = '';
+    for (let i = 0; i < dataLength; i++) {
+        let str = data[i].level;
+        listHtml += `
+            <div class="recordBar ${bmiLevelClass[str].class} h-bg-white">
+                <span class="recordBar__item h3">${bmiLevelClass[str].message}</span>
+                <span class="recordBar__item h3 bmi">${data[i].bmi}</span>
+                <span class="recordBar__item h3 weight">${data[i].weight}</span>
+                <span class="recordBar__item h3 height">${data[i].height}</span>
+                <span class="recordBar__item h6">${data[i].time}</span>
+                <a href="#" class="fas fa-trash-alt" id="dellItem" data-num="${[i]}"></a>
             </div>
             `;
-        }
-        else if (data[i].level === '過重') {
-            listHtml += `
-            <div class="recordBar recordBar--alert-3 h-bg-white">
-                <span class="recordBar__item h3">`+ data[i].level + `</span>
-                <span class="recordBar__item h3 bmi">`+ data[i].bmi + `</span>
-                <span class="recordBar__item h3 weight">`+ data[i].weight + `</span>
-                <span class="recordBar__item h3 height">`+ data[i].height + `</span>
-                <span class="recordBar__item h6">`
-                +(today.getMonth() + 1) + '-' + today.getDate() + '-' + today.getFullYear()+
-                `</span>
-            </div>
-            `;
-        }
-        else if (data[i].level === '輕度肥胖') {
-            listHtml += `
-            <div class="recordBar recordBar--alert-4 h-bg-white">
-                <span class="recordBar__item h3">`+ data[i].level + `</span>
-                <span class="recordBar__item h3 bmi">`+ data[i].bmi + `</span>
-                <span class="recordBar__item h3 weight">`+ data[i].weight + `</span>
-                <span class="recordBar__item h3 height">`+ data[i].height + `</span>
-                <span class="recordBar__item h6">`
-                +(today.getMonth() + 1) + '-' + today.getDate() + '-' + today.getFullYear()+
-                `</span>
-            </div>
-            `;
-        }
-        else if (data[i].level === '中度肥胖') {
-            listHtml += `
-            <div class="recordBar recordBar--alert-4 h-bg-white">
-                <span class="recordBar__item h3">`+ data[i].level + `</span>
-                <span class="recordBar__item h3 bmi">`+ data[i].bmi + `</span>
-                <span class="recordBar__item h3 weight">`+ data[i].weight + `</span>
-                <span class="recordBar__item h3 height">`+ data[i].height + `</span>
-                <span class="recordBar__item h6">`
-                +(today.getMonth() + 1) + '-' + today.getDate() + '-' + today.getFullYear()+
-                `</span>
-            </div>
-            `;
-        }
-        else if (data[i].level === '重度肥胖') {
-            listHtml += `
-            <div class="recordBar recordBar--alert-5 h-bg-white">
-                <span class="recordBar__item h3">`+ data[i].level + `</span>
-                <span class="recordBar__item h3 bmi">`+ data[i].bmi + `</span>
-                <span class="recordBar__item h3 weight">`+ data[i].weight + `</span>
-                <span class="recordBar__item h3 height">`+ data[i].height + `</span>
-                <span class="recordBar__item h6">`
-                +(today.getMonth() + 1) + '-' + today.getDate() + '-' + today.getFullYear()+
-                `</span>
-            </div>
-            `;
-        }
-        else {
-            listHtml += `
-            <div class="recordBar recordBar--alert-1 h-bg-white">
-                <span class="recordBar__item h3">`+ data[i].level + `</span>
-                <span class="recordBar__item h3 bmi">`+ data[i].bmi + `</span>
-                <span class="recordBar__item h3 weight">`+ data[i].weight + `</span>
-                <span class="recordBar__item h3 height">`+ data[i].height + `</span>
-                <span class="recordBar__item h6">`
-                +(today.getMonth() + 1) + '-' + today.getDate() + '-' + today.getFullYear()+
-                `</span>
-            </div>
-            `;
-        }
     }
     if (dataLength !== 0) { del.classList.remove('h-d-none') }
     list.innerHTML = listHtml;
 }
 
+// event init
+listHtml(data);
+
+// event
+function eventDate(e) {
+    if (e.target.id === 'resultValue') {
+        e.preventDefault();
+        let w = parseInt(document.querySelector('#weightId').value);
+        let h = parseInt(document.querySelector('#heightId').value);
+        if (isNaN(w) || isNaN(h)) {
+            alert('請輸入數值哦 ( ˘•ω•˘ ).oOஇ')
+            return
+        };
+        let bmi = bmiCount(w, h);
+        let today = new Date();
+        let time = `${(today.getMonth() + 1)}-${today.getDate()}-${today.getFullYear()}`;
+        upDateArray(w, h, bmi, time);
+        resultHtml(bmi);
+        listHtml(data);
+    }
+    else if (e.target.id === 'resetValue') {
+        resetHtml();
+    }
+}
+
+function delData(e) {
+    e.preventDefault();
+    let dataLength = data.length;
+    if (e.target.id !== 'dellItem') { return };
+    let num = e.target.dataset.num;
+    data.splice(num, 1);
+    if (data.length == 0) { del.classList.add('h-d-none') }
+    localStorage.setItem('listData', JSON.stringify(data));
+    listHtml(data);
+}
+
+function delallData(e) {
+    e.preventDefault();
+    data = [];
+    del.classList.add('h-d-none');
+    localStorage.setItem('listData', JSON.stringify(data));
+    listHtml(data);
+}
+
+countId.addEventListener('click', eventDate, false);
+list.addEventListener('click', delData, false);
+del.addEventListener('click', delallData, false);
